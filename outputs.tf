c9df0f354987dc246683685b0f73d3096f6a5e98
Adnan <adnanshaikh11501@gmail.com>
2024-01-31 15:51:40 +0530

    public hosted zone

--- outputs.tf
+++ outputs.tf
@@ -14,9 +14,9 @@ output "PRIVATE_ALB_ADDRESS" {
   value = module.alb-private.ALB_ADDRESS
 }
 
-# output "PUBLIC_LISTENER_ARN" {
-#   value = module.alb-public.LISTENER_ARN[0]                                
-# }
+output "PUBLIC_LISTENER_ARN" {
+  value = module.alb-public.LISTENER_ARN[0]                                
+}
 
 output "PRIVATE_LISTENER_ARN" {
   value = module.alb-private.LISTENER_ARN[0]                                
